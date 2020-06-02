74ddbf0335266234741103be7b239ee2b5f180ea
George Thomas <georgefsthomas@gmail.com>
2020-06-02 20:28:19 +0100

    Use lenses rather than record extensions
    What we really want is RecordDotSyntax...

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,22 @@
+
 {- TODO
-use lenses (since svg-tree is clearly set up that way)
 remove all incomplete pattern matches etc. one way or another
 may not work correctly if height attribute not equal to viewbox height (ditto width)
 auto-scale
 dpi setting doesn't seem to make a difference to the output
 what does 'extra' field in sporcle table even do?
-better way of allowing user to enter name, hint, extra
 empty names, hints, extra seem to mess things up (actually likely a bug in sporcle's parser)
 -}
 
 import Codec.Picture
 import Control.Monad
+import Data.Generics.Labels ()
 import Data.List.Extra
 import Data.Maybe
 import Graphics.Rasterific.Svg
 import Graphics.Svg
 import Graphics.Text.TrueType
+import Lens.Micro
 import Linear.V2
 import Options.Generic
 import Text.Pretty.Simple
@@ -36,52 +37,55 @@ data Entry = Entry
       shape :: [V2 Int],
       answerPos :: V2 Int
     }
+    deriving (Generic)
 
 data MetaData = MetaData
     { hint' :: Maybe String,
       answer' :: Maybe String
     }
+    deriving (Generic)
 
 main :: IO ()
 main = do
-    Args {..} <- getRecord "Sporcle picture click SVG helper"
-    Just doc <- loadSvgFile inSvg
-    when debug $ pPrint doc
-    writePng outPng =<< fst <$> renderSvgDocument emptyFontCache Nothing dpi doc
-    writeFile outSporcle $ unlines $ map render $ convertDoc doc
+    (args :: Args) <- getRecord "Sporcle picture click SVG helper"
+    Just doc <- loadSvgFile $ args ^. #inSvg
+    when (args ^. #debug) $ pPrint doc
+    writePng (args ^. #outPng) =<< fst <$> renderSvgDocument emptyFontCache Nothing (args ^. #dpi) doc
+    writeFile (args ^. #outSporcle) $ unlines $ map render $ convertDoc doc
 
 convertDoc :: Document -> [Entry]
-convertDoc Document {..} = map (makeEntry . convertElem (V2 x y)) _elements
+convertDoc doc = map (uncurry makeEntry . convertElem (V2 x y)) $ doc ^. elements
     where
-        Just (x, y, _, _) = _viewBox
+        Just (x, y, _, _) = doc ^. viewBox
 
-makeEntry :: (MetaData, [V2 Double]) -> Entry
-makeEntry (MetaData {..}, vs) = Entry {..}
-    where
-        answer = fromMaybe "answer" answer'
-        hint = fromMaybe "hint" hint'
-        extra = "extra"
-        shape = round <<$>> vs
-        answerPos = round <$> mean vs
+makeEntry :: MetaData -> [V2 Double] -> Entry
+makeEntry m vs =
+    Entry
+        { answer = fromMaybe "answer" $ m ^. #answer',
+          hint = fromMaybe "hint" $ m ^. #hint',
+          extra = "extra",
+          shape = round <<$>> vs,
+          answerPos = round <$> mean vs
+        }
 
 convertElem :: V2 Double -> Tree -> (MetaData, [V2 Double])
 convertElem v = \case
-    GroupTree Group {..} -> case _groupChildren of
-        [PathTree Path {..}] ->
-            (parseMetaData _attrId, map (subtract v) $ convertPath _pathDefinition)
-            where
-                DrawAttributes {..} = _pathDrawAttributes
+    GroupTree g -> case g ^. groupChildren of
+        [PathTree p] ->
+            ( maybe (MetaData Nothing Nothing) parseMetaData $ p ^. pathDrawAttributes . attrId,
+              map (subtract v) $ convertPath $ p ^. pathDefinition
+            )
 
 -- read from a path's id tag
 -- sticking to the sporcle convention, we separate by tab
---TODO this could probably be more concise
-parseMetaData :: Maybe String -> MetaData
-parseMetaData = (\(hint', answer') -> MetaData {..}) . \case
-    Just s -> case splitOn "\t" s of
-        [x1, x2] -> (Just x1, Just x2)
-        [x] -> (Just x, Just x)
-        [] -> (Nothing, Nothing)
-    Nothing -> (Nothing, Nothing)
+parseMetaData :: String -> MetaData
+parseMetaData s = MetaData {hint', answer'}
+    where
+        (hint', answer') =
+            case splitOn "\t" s of
+                [x1, x2] -> (Just x1, Just x2)
+                [x] -> (Just x, Just x)
+                [] -> (Nothing, Nothing)
 
 -- expects a MoveTo, several LineTo, then an EndPath
 convertPath :: [PathCommand] -> [V2 Double]
@@ -93,14 +97,14 @@ convertPath = \case
             [EndPath] -> []
 
 render :: Entry -> String
-render Entry {..} =
+render e =
     intercalate
         "\t"
-        [ hint,
-          answer,
-          extra,
-          intercalate "; " $ map vec shape,
-          vec answerPos
+        [ e ^. #hint,
+          e ^. #answer,
+          e ^. #extra,
+          intercalate "; " $ map vec $ e ^. #shape,
+          vec $ e ^. #answerPos
         ]
     where
         vec (V2 x y) = show x <> "," <> show y
