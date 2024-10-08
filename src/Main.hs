d9ba5722e18dc2692b1653ba655c95a6ee5b9d85
George Thomas <georgefsthomas@gmail.com>
2024-10-08 12:49:07 +0100

    Run Fourmolu

--- src/Main.hs
+++ src/Main.hs
@@ -39,21 +39,22 @@ main = handle (\(e :: IOError) -> printError (show e) >> exitFailure) $ do
 
 allShapes :: Document -> M [Shape]
 allShapes doc = do
-    trans <- second . map . subtract <$> case doc._viewBox of
-        Just (x, y, w, h) -> do
-            --TODO we ought to actually be able to adjust to these not being equal
-            when (doc._width /= Just (Num w)) $ warn "width attribute not equal to viewbox width" (w, doc._width)
-            when (doc._height /= Just (Num h)) $ warn "height attribute not equal to viewbox height" (h, doc._height)
-            return $ V2 x y
-        Nothing -> warn "SVG has no viewbox" () >> return (V2 1920 1080)
+    trans <-
+        second . map . subtract <$> case doc._viewBox of
+            Just (x, y, w, h) -> do
+                -- TODO we ought to actually be able to adjust to these not being equal
+                when (doc._width /= Just (Num w)) $ warn "width attribute not equal to viewbox width" (w, doc._width)
+                when (doc._height /= Just (Num h)) $ warn "height attribute not equal to viewbox height" (h, doc._height)
+                return $ V2 x y
+            Nothing -> warn "SVG has no viewbox" () >> return (V2 1920 1080)
     concat <$> sequence [uncurry makeShape . trans <<$>> treePaths e | e <- doc._elements]
 
 makeShape :: MetaData -> [V2 Double] -> Shape
 makeShape meta vs =
     Shape
-        { meta,
-          shape = round <<$>> vs,
-          answerPos = round <$> mean vs
+        { meta
+        , shape = round <<$>> vs
+        , answerPos = round <$> mean vs
         }
 
 treePaths :: Tree -> M [(MetaData, [V2 Double])]
@@ -67,92 +68,94 @@ treePaths = \case
     SymbolTree (Symbol g) -> concat <$> mapM treePaths g._groupChildren
     UseTree _ (Just t) -> treePaths t
     _ -> return []
-    where
-        def =
-            MetaData
-                { hint = "hint",
-                  answer = "answer",
-                  extra = "extra"
-                }
+  where
+    def =
+        MetaData
+            { hint = "hint"
+            , answer = "answer"
+            , extra = "extra"
+            }
 
---TODO implementation is quite odd - I'm sure it could be simpler
+-- TODO implementation is quite odd - I'm sure it could be simpler
 -- read from a path's 'id' tag
 -- sticking to the sporcle convention, we separate by tab
 parseMetaData :: String -> M MetaData
 parseMetaData s = do
     (hint, (answer, extra)) <- case uncons' $ splitOn "\t" s of
-        (x0, xs0) -> (fromMaybe "hint" x0,) <$> case uncons' xs0 of
-            (x1, xs1) -> (fromMaybe "answer" x1,) <$> case uncons' xs1 of
-                (x2, xs2) -> do
-                    unless (null xs2) $ warn "Failed to fully parse metadata (more than one tab character)" s
-                    return $ fromMaybe "extra" x2
-    return $ MetaData {hint, answer, extra}
-    where
-        uncons' = maybe (Nothing, []) (first Just) . uncons
+        (x0, xs0) ->
+            (fromMaybe "hint" x0,) <$> case uncons' xs0 of
+                (x1, xs1) ->
+                    (fromMaybe "answer" x1,) <$> case uncons' xs1 of
+                        (x2, xs2) -> do
+                            unless (null xs2) $ warn "Failed to fully parse metadata (more than one tab character)" s
+                            return $ fromMaybe "extra" x2
+    return $ MetaData{hint, answer, extra}
+  where
+    uncons' = maybe (Nothing, []) (first Just) . uncons
 
 -- expects a 'MoveTo', several 'LineTo', then an 'EndPath'
 extractPath :: [PathCommand] -> Either String [V2 Double]
 extractPath = \case
     MoveTo OriginAbsolute [v] : cs -> (v :) <$> f cs
     _ -> Left "Illegal start of path"
-    where
-        f = \case
-            LineTo OriginAbsolute [v] : cs -> (v :) <$> f cs
-            [EndPath] -> pure []
-            _ -> Left "Malformed path"
+  where
+    f = \case
+        LineTo OriginAbsolute [v] : cs -> (v :) <$> f cs
+        [EndPath] -> pure []
+        _ -> Left "Malformed path"
 
 render :: Shape -> String
 render e =
     intercalate
         "\t"
-        [ e.meta.hint,
-          e.meta.answer,
-          e.meta.extra,
-          intercalate "; " $ map vec e.shape,
-          vec e.answerPos
+        [ e.meta.hint
+        , e.meta.answer
+        , e.meta.extra
+        , intercalate "; " $ map vec e.shape
+        , vec e.answerPos
         ]
-    where
-        vec (V2 x y) = show x <> "," <> show y
+  where
+    vec (V2 x y) = show x <> "," <> show y
 
 data Args = Args
-    { inSvg :: FilePath,
-      outPng :: FilePath,
-      outSporcle :: FilePath,
-      dpi :: Maybe Int,
-      debug :: Bool
+    { inSvg :: FilePath
+    , outPng :: FilePath
+    , outSporcle :: FilePath
+    , dpi :: Maybe Int
+    , debug :: Bool
     }
     deriving (Generic, ParseRecord)
 
 data Shape = Shape
-    { meta :: MetaData,
-      shape :: [V2 Int],
-      answerPos :: V2 Int
+    { meta :: MetaData
+    , shape :: [V2 Int]
+    , answerPos :: V2 Int
     }
     deriving (Generic)
 
 data MetaData = MetaData
-    { hint :: String,
-      answer :: String,
-      extra :: String
+    { hint :: String
+    , answer :: String
+    , extra :: String
     }
     deriving (Generic)
 
 {- Our main monad -}
---TODO there would be benefits to making this more abstract
+-- TODO there would be benefits to making this more abstract
 
 type M a = Writer [Warning] a
 
 data Warning where
-    Warning :: Show a => String -> a -> Warning
+    Warning :: (Show a) => String -> a -> Warning
 
-warn :: Show a => String -> a -> M ()
+warn :: (Show a) => String -> a -> M ()
 warn = tell . pure .: Warning
 
 {- Util -}
 
---TODO centroid would be preferable - although in practice we'll aften manually adjust anyway
+-- TODO centroid would be preferable - although in practice we'll aften manually adjust anyway
 -- besides, this point is used for top-left of text box rather than centre
-mean :: Fractional a => [a] -> a
+mean :: (Fractional a) => [a] -> a
 mean xs = sum xs / fromIntegral (length xs)
 
 infixl 4 <<$>>
